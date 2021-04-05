0ae1adefd942cd6dd459749a0bc3be31061a3327
nhurtadoc <njhurtadoc7@hotmail.com>
2021-04-04 20:10:28 -0500

    modificaciones del trabajo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-apiKey='K1Cs7M3aHOiT5TPYph5YQMjvZEbaRLGL' ;
+const apiKey='K1Cs7M3aHOiT5TPYph5YQMjvZEbaRLGL' ;
+const autocomplete = document.getElementById('autocomplete');
+const suggestions1 = document.getElementById('suggestions1');
+const suggestions2 = document.getElementById('suggestions2');
+const hashtags = document.getElementById('hashtags');
+
 async function searching() {
     let valor = document.getElementById("search").value;
     if (valor !=''){
+        document.getElementById('suggestions').style.display = 'none';
         document.getElementById('list-images').innerHTML ='';
         let result= await getSearchResults (valor);
         console.log (result);
@@ -14,8 +20,20 @@ async function searching() {
 
             document.getElementById('search-trendings').placeholder = document.getElementById('search').value;
         }
+        hashtagsSuggestions(valor);
     }
 }
+
+document.getElementById('search').addEventListener('input', (event) => {
+    if (event.target.value.length > 0) {
+        document.getElementById('suggestions').style.display = 'block';
+        suggestions(event.target.value);
+    }else {
+        document.getElementById('suggestions').style.display = 'none';
+
+    }
+});
+
 function getSearchResults(search) {   
     const found = fetch('http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=' + apiKey)
     .then((response) => {
@@ -24,6 +42,73 @@ function getSearchResults(search) {
     .catch((error) => {return error});
     return found 
 } 
+
+function suggestions(text) {
+    const found = fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=56cTthKcllF5tck39cR59sP4wXo8fp5q&q=${text}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(function (json) {
+            if (json.data.length == 1) {
+                autocomplete.innerHTML = json.data[0].name;
+                suggestions1.innerHTML = text;
+                suggestions2.innerHTML = text;
+            } else if (json.data.length == 2) {
+                autocomplete.innerHTML = json.data[0].name;
+                suggestions1.innerHTML = json.data[1].name;
+                suggestions2.innerHTML = text;
+            } else if (json.data.length > 2) {
+                autocomplete.innerHTML = json.data[0].name;
+                suggestions1.innerHTML = json.data[1].name;
+                suggestions2.innerHTML = json.data[2].name;
+            } else {
+                autocomplete.innerHTML = text;
+                suggestions1.innerHTML = text;
+                suggestions2.innerHTML = text;
+            }
+            document.getElementById("list-suggestions").addEventListener("click", function(e) {
+                document.getElementById("search").value = e.target.innerText;
+                searching();
+            })
+        })
+        .catch(error => {
+            return error;
+        });
+}
+
+function hashtagsSuggestions(text) {
+    const found = fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=56cTthKcllF5tck39cR59sP4wXo8fp5q&q=${text}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log('result',json)
+            hashtags.innerHTML = "";
+            let button = "";
+
+            for (let i=0 ; i < json.data.length; i++) {
+                let current = json.data[i];
+                let nameH = "#" + current.name.replaceAll(" ","");
+                let x = document.createElement("BUTTON");
+                let t = document.createTextNode(nameH);
+                x.appendChild(t);
+
+               //button += '<button class ="hashtag" value = "'+ current.name +'">' + nameH +'</button>';
+                x.className = 'hashtags';
+                x.setAttribute("value", current.name);
+                x.onclick = function(){
+                    document.getElementById("search").value = current.name;
+                    searching();
+                }
+                hashtags.append(x);
+            }
+            
+            
+        })
+        .catch(error => {
+            return error;
+        });
+}
 
 function create() {
     location.href='create.html'
